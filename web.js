1a30db15b91914e04ab492177a88a88e51e05923
Daniel Sun <xdansolo32@hotmail.com>
2013-02-26 01:55:29 -0800

    wrong redirect url, fucking fb

--- web.js
+++ web.js
@@ -30,7 +30,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: "http://dry-atoll-1583.herokuapp.com/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // asynchronous verification, for effect...
